5f1691f50d0e6a9b96f62b485cb37bb7876f1c89
dianjixz <18637716021@163.com>
2024-12-20 14:33:02 +0800

    [update]

--- tools/creat_AX630C_LITE_ubuntu22_04_image.sh
+++ tools/creat_AX630C_LITE_ubuntu22_04_image.sh
@@ -7,6 +7,7 @@ if [ -z "${EXT_ROOTFS_SIZE}" ]; then
     export EXT_ROOTFS_SIZE=30606884864
 fi
 
+
 [ -d 'build_AX630C_LITE_ubuntu22_04' ] || mkdir -p build_AX630C_LITE_ubuntu22_04/ubuntu-base-22.04.5-base-arm64
 ./creat_AX630C_LITE_buidlroot_image.sh && sudo cp build_AX630C_LITE_buidlroot/buildroot/output/axera-image build_AX630C_LITE_ubuntu22_04/ -a
 [ -d 'build_AX630C_LITE_ubuntu22_04/axera-image' ] || { echo "not found axera-image" && exit -1; }
@@ -28,13 +29,13 @@ sudo chroot ubuntu-base-22.04.5-base-arm64/ /bin/bash -c 'echo "root:root" | chp
 sudo rm rootfs/etc/apt/sources.list && sudo touch rootfs/etc/apt/sources.list
 
 sudo echo "deb [trusted=yes] file:/var/deb-archives ./" > rootfs/etc/apt/sources.list.d/local-repo.list
-sudo chroot rootfs/ /bin/bash -c 'apt update ; echo "tzdata tzdata/Areas select Asia" | debconf-set-selections ; echo "tzdata tzdata/Zones/Asia select Shanghai" | debconf-set-selections ; DEBIAN_FRONTEND=noninteractive apt install vim net-tools network-manager -y'
-
+sudo chroot rootfs/ /bin/bash -c 'apt update ; echo "tzdata tzdata/Areas select Asia" | debconf-set-selections ; echo "tzdata tzdata/Zones/Asia select Shanghai" | debconf-set-selections ; DEBIAN_FRONTEND=noninteractive apt install vim net-tools network-manager i2c-tools lrzsz kmod iputils-ping openssh-server ifplugd -y --option=Dpkg::Options::="--force-confnew"'
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' rootfs/etc/ssh/sshd_config
 sudo rm rootfs/etc/apt/sources.list.d/local-repo.list
 sudo cp rootfs/etc/apt/sources.list.bak rootfs/etc/apt/sources.list
 sudo sed -i '1a 127.0.0.1       m5stack-LLM' rootfs/etc/hosts
 sudo rm rootfs/var/deb-archives -rf
-
+# sudo rm rootfs/etc/modprobe.d/blacklist* -rf
 
 sudo cp ../../board/m5stack/overlay/usr/* rootfs/usr/ -a
 
@@ -43,13 +44,14 @@ sudo simg2img rootfs_sparse.ext4 rootfs_.ext4
 mkdir build_rootfs
 sudo mount rootfs_.ext4 build_rootfs
 
-
-
 sudo cp build_rootfs/lib/modules rootfs/lib/ -a
 sudo cp build_rootfs/lib/firmware/* rootfs/lib/firmware/ -a
+
+sudo rm rootfs/usr/bin/sh -f
+sudo cp build_rootfs/bin/busybox rootfs/usr/bin/ -a
 sudo cp build_rootfs/sbin/devmem rootfs/usr/sbin/ -a
-
-
+sudo cp build_rootfs/bin/sh rootfs/usr/bin/ -a
+sudo cp build_rootfs/usr/lib/libcrypto.so.1.1 rootfs/usr/lib/ -a
 
 sudo umount build_rootfs
 sudo rm build_rootfs rootfs_sparse.ext4 rootfs_.ext4 -rf
@@ -61,6 +63,7 @@ sudo tar zxf ../../board/m5stack/soc.tar.gz -C rootfs/soc
 
 sudo find rootfs -name ".empty" -exec rm {} -f \;
 
+sudo rm axera-image/rootfs_sparse.ext4
 sudo ../bin/make_ext4fs -l ${EXT_ROOTFS_SIZE} -s axera-image/rootfs_sparse.ext4 ubuntu-base-22.04.5-base-arm64/
 
 cd axera-image
--- tools/creat_Module_LLM_ubuntu22_04_image.sh
+++ tools/creat_Module_LLM_ubuntu22_04_image.sh
@@ -27,42 +27,41 @@ sudo chroot ubuntu-base-22.04.5-base-arm64/ /bin/bash -c 'echo "root:root" | chp
 sudo rm rootfs/etc/apt/sources.list && sudo touch rootfs/etc/apt/sources.list
 
 sudo echo "deb [trusted=yes] file:/var/deb-archives ./" > rootfs/etc/apt/sources.list.d/local-repo.list
-sudo chroot rootfs/ /bin/bash -c 'apt update ; echo "tzdata tzdata/Areas select Asia" | debconf-set-selections ; echo "tzdata tzdata/Zones/Asia select Shanghai" | debconf-set-selections ; DEBIAN_FRONTEND=noninteractive apt install vim net-tools network-manager -y'
-
+sudo chroot rootfs/ /bin/bash -c 'apt update ; echo "tzdata tzdata/Areas select Asia" | debconf-set-selections ; echo "tzdata tzdata/Zones/Asia select Shanghai" | debconf-set-selections ; DEBIAN_FRONTEND=noninteractive apt install vim net-tools network-manager i2c-tools lrzsz kmod iputils-ping openssh-server ifplugd -y --option=Dpkg::Options::="--force-confnew"'
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' rootfs/etc/ssh/sshd_config
 sudo rm rootfs/etc/apt/sources.list.d/local-repo.list
 sudo cp rootfs/etc/apt/sources.list.bak rootfs/etc/apt/sources.list
 sudo sed -i '1a 127.0.0.1       m5stack-LLM' rootfs/etc/hosts
 sudo rm rootfs/var/deb-archives -rf
-
-sudo rm axera-image/rootfs_sparse.ext4
-
-
-sudo tar zxf ../../board/m5stack/soc.tar.gz -C rootfs/soc
-[ -f "../../board/m5stack/opt.tar.gz" ] && sudo tar zxf ../../board/m5stack/opt.tar.gz -C rootfs/opt
-
-
+# sudo rm rootfs/etc/modprobe.d/blacklist* -rf
 
 sudo cp ../../board/m5stack/overlay/usr/* rootfs/usr/ -a
-sudo cp ../../board/m5stack/module_LLM/overlay/usr/* rootfs/usr/ -a
 
 sudo cp axera-image/rootfs_sparse.ext4 rootfs_sparse.ext4
 sudo simg2img rootfs_sparse.ext4 rootfs_.ext4
 mkdir build_rootfs
 sudo mount rootfs_.ext4 build_rootfs
 
-
-
 sudo cp build_rootfs/lib/modules rootfs/lib/ -a
 sudo cp build_rootfs/lib/firmware/* rootfs/lib/firmware/ -a
+
+sudo rm rootfs/usr/bin/sh -f
+sudo cp build_rootfs/bin/busybox rootfs/usr/bin/ -a
 sudo cp build_rootfs/sbin/devmem rootfs/usr/sbin/ -a
-
-
+sudo cp build_rootfs/bin/sh rootfs/usr/bin/ -a
+sudo cp build_rootfs/usr/lib/libcrypto.so.1.1 rootfs/usr/lib/ -a
 
 sudo umount build_rootfs
 sudo rm build_rootfs rootfs_sparse.ext4 rootfs_.ext4 -rf
 
 
 
+sudo tar zxf ../../board/m5stack/soc.tar.gz -C rootfs/soc
+[ -f "../../board/m5stack/opt.tar.gz" ] && sudo tar zxf ../../board/m5stack/opt.tar.gz -C rootfs/opt
+
+sudo find rootfs -name ".empty" -exec rm {} -f \;
+
+sudo rm axera-image/rootfs_sparse.ext4
 sudo ../bin/make_ext4fs -l ${EXT_ROOTFS_SIZE} -s axera-image/rootfs_sparse.ext4 ubuntu-base-22.04.5-base-arm64/
 
 cd axera-image
