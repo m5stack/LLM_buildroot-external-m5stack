097f63bd58fd4b0957ecff668b61021c4ba67367
dianjixz <18637716021@163.com>
2025-07-15 18:52:08 +0800

    [update] kernel uboot

--- boot/uboot/uboot.mk
+++ boot/uboot/uboot.mk
@@ -2,9 +2,9 @@
 #	$(Q)cp $(BR2_EXTERNAL_M5STACK_PATH)/board/m5stack/module-LLM/pinmux/* $(UBOOT_DIR)/board/axera/ax620e_emmc/
 #endef
 #UBOOT_PRE_BUILD_HOOKS += MY_UBOOT_PRE_BUILD_HOOK
+BR2_BOARD_M5STACK_NAME_STR=$(subst ",,$(BR2_BOARD_M5STACK_NAME))
 
-
-define MY_UBOOT_POST_INSTALL_IMAGES_HOOK
+define AX630C_emmc_arm64_k419_UBOOT_POST_INSTALL_IMAGES_HOOK
 	python3 $(SIGN_SCRIPT) -i $(BINARIES_DIR)/u-boot-dtb.bin \
 		-o $(BINARIES_DIR)/fdl2_signed.bin -pub $(PUB_KEY) -prv $(PRIV_KEY) $(SIGN_PARAMS)
 	$(BR2_EXTERNAL_M5STACK_PATH)/tools/bin/ax_gzip -9 $(BINARIES_DIR)/u-boot-dtb.bin
@@ -12,7 +12,17 @@ define MY_UBOOT_POST_INSTALL_IMAGES_HOOK
 		-o $(BINARIES_DIR)/u-boot_signed.bin -pub $(PUB_KEY) -prv $(PRIV_KEY) $(SIGN_PARAMS)
 	cp $(BINARIES_DIR)/u-boot_signed.bin $(BINARIES_DIR)/u-boot_b_signed.bin
 endef
-UBOOT_POST_INSTALL_IMAGES_HOOKS += MY_UBOOT_POST_INSTALL_IMAGES_HOOK
+
+
+define AX650_emmc_UBOOT_POST_INSTALL_IMAGES_HOOK
+	python3 $(SPL_SIGN_SCRIPT) -i $(BINARIES_DIR)/u-boot-dtb.bin \
+		-o $(BINARIES_DIR)/u-boot_signed.bin -ob $(BINARIES_DIR)/uboot_bk.bin -pub $(PUB_KEY) -prv $(PRIV_KEY) $(SPL_SIGN_PARAMS)
+	python3 $(FDL_SIGN_SCRIPT) -i $(BINARIES_DIR)/u-boot-dtb.bin \
+		-o $(BINARIES_DIR)/fdl2_signed.bin -pub $(PUB_KEY) -prv $(PRIV_KEY) $(FDL_SIGN_PARAMS)
+endef
+
+
+UBOOT_POST_INSTALL_IMAGES_HOOKS += $(BR2_BOARD_M5STACK_NAME_STR)_UBOOT_POST_INSTALL_IMAGES_HOOK
 
 UBOOT_MAKE_TARGET := $(filter-out all,$(UBOOT_MAKE_TARGET))
 
--- linux/linux.mk
+++ linux/linux.mk
@@ -5,16 +5,26 @@
 # LINUX_PRE_BUILD_HOOKS += MY_LINUX_PRE_BUILD_HOOK
 
 # LINUX_ARCH_PATH = $(LINUX_DIR)/build/linux-4.19.125/arch/arm64
+BR2_BOARD_M5STACK_NAME_STR=$(subst ",,$(BR2_BOARD_M5STACK_NAME))
+BR2_LINUX_KERNEL_INTREE_DTS_NAME_STR=$(subst ",,$(BR2_LINUX_KERNEL_INTREE_DTS_NAME))
 
-
-define MY_LINUX_TARGET_FINALIZE_HOOK
+define AX630C_emmc_arm64_k419_LINUX_TARGET_FINALIZE_HOOK
 	if [[ "$(BINARIES_DIR)/Image.axgzip" -ot "$(BINARIES_DIR)/Image" ]] ; then $(BR2_EXTERNAL_M5STACK_PATH)/tools/bin/ax_gzip -9 $(BINARIES_DIR)/Image ;fi
-	if [[ "$(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME).dtb.axgzip" -ot "$(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME).dtb" ]] ; then $(BR2_EXTERNAL_M5STACK_PATH)/tools/bin/ax_gzip -9 $(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME).dtb ;fi
+	if [[ "$(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME_STR).dtb.axgzip" -ot "$(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME_STR).dtb" ]] ; then $(BR2_EXTERNAL_M5STACK_PATH)/tools/bin/ax_gzip -9 $(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME_STR).dtb ;fi
 	if [[ "$(BINARIES_DIR)/boot_signed.bin" -ot "$(BINARIES_DIR)/Image.axgzip" ]] ; then python3 $(SIGN_SCRIPT) -i $(BINARIES_DIR)/Image.axgzip \
 		-o $(BINARIES_DIR)/boot_signed.bin -pub $(PUB_KEY) -prv $(PRIV_KEY) $(SIGN_PARAMS) ;fi
-	if [[ "$(BINARIES_DIR)/AX630C_emmc_arm64_k419_signed.dtb" -ot "$(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME).dtb.axgzip" ]] ; then python3 $(SIGN_SCRIPT) -i $(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME).dtb.axgzip \
-		-o $(BINARIES_DIR)/AX630C_emmc_arm64_k419_signed.dtb -pub $(PUB_KEY) -prv $(PRIV_KEY) $(SIGN_PARAMS) ;fi
+	if [[ "$(BINARIES_DIR)/$(BR2_BOARD_M5STACK_NAME_STR)_signed.dtb" -ot "$(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME_STR).dtb.axgzip" ]] ; then python3 $(SIGN_SCRIPT) -i $(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME_STR).dtb.axgzip \
+		-o $(BINARIES_DIR)/$(BR2_BOARD_M5STACK_NAME_STR)_signed.dtb -pub $(PUB_KEY) -prv $(PRIV_KEY) $(SIGN_PARAMS) ;fi
 	cp $(BINARIES_DIR)/boot_signed.bin $(BINARIES_DIR)/boot_signed.bin.1
-	cp $(BINARIES_DIR)/AX630C_emmc_arm64_k419_signed.dtb $(BINARIES_DIR)/AX630C_emmc_arm64_k419_signed.dtb.1
+	cp $(BINARIES_DIR)/$(BR2_BOARD_M5STACK_NAME_STR)_signed.dtb $(BINARIES_DIR)/$(BR2_BOARD_M5STACK_NAME_STR)_signed.dtb.1
 endef
-LINUX_TARGET_FINALIZE_HOOKS += MY_LINUX_TARGET_FINALIZE_HOOK
+
+define AX650_emmc_LINUX_TARGET_FINALIZE_HOOK
+	python3 $(SEC_SIGN_SCRIPT) -i $(BINARIES_DIR)/Image -pub $(PUB_KEY) -prv $(PRIV_KEY)  -o $(BINARIES_DIR)/boot_signed.bin $(SEC_SIGN_PARAMS) 
+	python3 $(SEC_SIGN_SCRIPT) -i $(BINARIES_DIR)/$(BR2_LINUX_KERNEL_INTREE_DTS_NAME_STR).dtb -pub $(PUB_KEY) -prv $(PRIV_KEY) -o $(BINARIES_DIR)/$(BR2_BOARD_M5STACK_NAME_STR)_signed.dtb $(SEC_SIGN_PARAMS) 
+	cp $(BINARIES_DIR)/boot_signed.bin $(BINARIES_DIR)/recovery_signed.bin
+	cp $(BINARIES_DIR)/$(BR2_BOARD_M5STACK_NAME_STR)_signed.dtb $(BINARIES_DIR)/recovery_signed.dtb
+endef
+
+
+LINUX_TARGET_FINALIZE_HOOKS += $(BR2_BOARD_M5STACK_NAME_STR)_LINUX_TARGET_FINALIZE_HOOK
